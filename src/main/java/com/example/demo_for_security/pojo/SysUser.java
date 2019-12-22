fb3c05df5fe340d17fbd6001450a2e5e0ed21372
2329385085@qq.com <2329385085@qq.com>
2019-12-22 18:36:06 +0800

    添加 安全系统用户接口实现

--- src/main/java/com/example/demo_for_security/pojo/SysUser.java
+++ src/main/java/com/example/demo_for_security/pojo/SysUser.java
@@ -1,5 +1,6 @@
 package com.example.demo_for_security.pojo;
 
+import com.lhit.starter.security.pojo.LhitSecurityUser;
 import lombok.Getter;
 import lombok.Setter;
 import tk.mybatis.mapper.annotation.KeySql;
@@ -11,7 +12,7 @@ import java.util.Date;
 @Getter
 @Setter
 @Table(name = "sys_user")
-public class SysUser {
+public class SysUser implements LhitSecurityUser {
     @Id
     @KeySql(useGeneratedKeys = true)
     private Long id;
@@ -32,4 +33,8 @@ public class SysUser {
 
     private Date lastLogin;
 
+    @Override
+    public String getUserId() {
+        return String.valueOf(id);
+    }
 }
